1e4492d3f0dc3aac090ac5d5c23fbac55a3d07c1
Nazarenko Bohdan <galaxy363691@gmail.com>
2025-11-01 19:07:12 +0100

    Add main.js and update project files

---
+++ main.js
@@ -0,0 +1,65 @@
+
+
+function outerFunction(arg1) {
+    return function innerFunction(arg2) {
+        return function deepInnerFunction(arg3) {
+            return arg1 * arg2 * arg3;
+        };
+    };
+}
+
+const result = outerFunction(2)(3)(4);
+console.log(result);
+
+export { outerFunction };
+
+function originalFunction(num) {
+    return num * num;
+}
+
+function modifyFunction(originalFunc, multiplier) {
+    return function(num) {
+        return originalFunc(num) * multiplier;
+    };
+}
+
+
+const modifiedFunc = modifyFunction(originalFunction, 3);
+console.log('Original function output for 4:', originalFunction(4));
+console.log('Modified function output for 4:', modifiedFunc(4));
+
+export { originalFunction, modifyFunction };
+
+
+function curriedDomain(protocol) {
+    return function(domainName) {
+        return function(tld) {
+            return `${protocol}://${domainName}.${tld}`;
+        };
+    };
+}
+
+// Приклад використання
+const protocolSetter = curriedDomain('https');
+const domainNameSetter = protocolSetter('example');
+const fullDomain = domainNameSetter('com'); //
+console.log('Full Domain:', fullDomain);
+
+export { curriedDomain };
+
+
+function curriedAdd(a) {
+    return function(b) {
+        return function(c) {
+            return a + b + c;
+        };
+    };
+}
+
+const addFirst = curriedAdd(1);
+const addSecond = addFirst(2);
+const result = addSecond(3);
+console.log('Result:', result);
+
+export { curriedAdd };
+
